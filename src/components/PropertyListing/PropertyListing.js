4bdd535d179c7cd8f185141b904a8c19a7935ad9
apoorva-saxena <apsaxena24@gmail.com>
2021-02-16 00:40:38 +0000

    Get properties from api and fix test

--- src/components/PropertyListing/PropertyListing.js
+++ src/components/PropertyListing/PropertyListing.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import PropertyCard from '../PropertyCard';
-import './PropertyListing.scss';
-
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
-};
+import React, { useEffect, useState } from "react";
+import PropertyCard from "../PropertyCard";
+import "./PropertyListing.scss";
 
 const PropertyListing = () => {
-    return (
-        <div className="PropertyListing">
-            {
-                Array(5).fill(DUMMY_PROPERTY)
-                    .map((property, index) => <PropertyCard key={index} {...property}/>)
-            }
-        </div>
-    )
+  const [properties, setProperties] = useState([]);
+
+  const getProperties = () => {
+    fetch("api/properties", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((properties) => {
+        setProperties(properties);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getProperties();
+  });
+
+  return (
+    <div className="PropertyListing">
+      {properties.length > 0 &&
+        properties.map((property, index) => (
+          <PropertyCard key={index} {...property} />
+        ))}
+    </div>
+  );
 };
 
 export default PropertyListing;
--- src/components/PropertyListing/tests/PropertyListing.spec.js
+++ src/components/PropertyListing/tests/PropertyListing.spec.js
@@ -9,8 +9,10 @@ describe('PropertyListing', () => {
         expect(wrapper.find('.PropertyListing')).toHaveLength(1);
     });
 
-    it('should render five property cards', () => {
+    it('should render 0 property cards', () => {
         const wrapper = shallow(<PropertyListing/>);
-        expect(wrapper.find('PropertyCard')).toHaveLength(5);
+        expect(wrapper.find('PropertyCard')).toHaveLength(0);
     });
+
+    
 });
